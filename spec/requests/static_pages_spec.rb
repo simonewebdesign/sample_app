8fb9e6878e9c7dfc194f6d4ce237651bcb753841
Simone <hello@simonewebdesign.it>
2012-12-04 13:54:32 +0100

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,19 +4,31 @@ describe "StaticPages" do
   describe "Home page" do
     it "should have the content 'Home page'" do
       visit '/static_pages/home'
-      page.should have_content('Home page')
+      page.should have_selector('h1', text: 'Home')
+    end
+    it "should have the right title" do
+      visit '/static_pages/home'
+      page.should have_selector('title', text: "Home | Ruby on Rails Tutorial Sample App")
     end
   end
   describe "Help page" do
     it "should have the content 'Help'" do
       visit '/static_pages/help'
-      page.should have_content('Help')
+      page.should have_selector('h1', text: 'Help')
     end
+    it "should have the right title" do
+      visit '/static_pages/help'
+      page.should have_selector('title', text: "Help | Ruby on Rails Tutorial Sample App")
+    end    
   end
   describe "About page" do
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
-      page.should have_content('About Us')
+      page.should have_selector('h1', text: 'About Us')
     end
-  end  
+    it "should have the right title" do
+      visit '/static_pages/about'
+      page.should have_selector('title', text: "About Us | Ruby on Rails Tutorial Sample App")
+    end    
+  end
 end
