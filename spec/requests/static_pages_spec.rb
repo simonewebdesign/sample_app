05327bdf2b4e88e76ffc428f198eb66fd01fff88
Simone <hello@simonewebdesign.it>
2012-12-04 12:30:22 +0100

    Create About page + specs

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+  describe "Home page" do
+    it "should have the content 'Home page'" do
+      visit '/static_pages/home'
+      page.should have_content('Home page')
+    end
+  end
+  describe "Help page" do
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      page.should have_content('Help')
+    end
+  end
+  describe "About page" do
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_content('About Us')
+    end
+  end  
+end
