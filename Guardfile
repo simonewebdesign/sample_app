2b25edd0073ec5a2cb1cf81fa9094755b3a4c800
Simone <hello@simonewebdesign.it>
2012-12-12 12:10:48 +0100

    Finish layout and routes
    End of chapter 5.

--- Guardfile
+++ Guardfile
@@ -39,17 +39,17 @@ guard 'rspec', version: 2, all_after_pass: false, cli: '--drb' do
 
 
   # added from ruby on rails tutorial
-    watch(%r{^app/controllers/(.+)_(controller)\.rb$})  do |m|
-    ["spec/routing/#{m[1]}_routing_spec.rb",
-     "spec/#{m[2]}s/#{m[1]}_#{m[2]}_spec.rb",
-     "spec/acceptance/#{m[1]}_spec.rb",
-     (m[1][/_pages/] ? "spec/requests/#{m[1]}_spec.rb" : 
-                       "spec/requests/#{m[1].singularize}_pages_spec.rb")]
-  end
-  watch(%r{^app/views/(.+)/}) do |m|
-    (m[1][/_pages/] ? "spec/requests/#{m[1]}_spec.rb" : 
-                       "spec/requests/#{m[1].singularize}_pages_spec.rb")
-  end
+  #  watch(%r{^app/controllers/(.+)_(controller)\.rb$})  do |m|
+  #  ["spec/routing/#{m[1]}_routing_spec.rb",
+  #   "spec/#{m[2]}s/#{m[1]}_#{m[2]}_spec.rb",
+  #   "spec/acceptance/#{m[1]}_spec.rb",
+  #   (m[1][/_pages/] ? "spec/requests/#{m[1]}_spec.rb" : 
+  #                     "spec/requests/#{m[1].singularize}_pages_spec.rb")]
+  #end
+  #watch(%r{^app/views/(.+)/}) do |m|
+  #  (m[1][/_pages/] ? "spec/requests/#{m[1]}_spec.rb" : 
+  #                     "spec/requests/#{m[1].singularize}_pages_spec.rb")
+  #end
 
 
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,51 +1,35 @@
 require 'spec_helper'
 
-describe "StaticPages" do
+describe "Static pages" do
 
-  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+  subject { page }
 
   describe "Home page" do
-    it "should have the content 'Home page'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1', text: 'Home')
-    end
-    it "should have the right title" do
-      visit '/static_pages/home'
-      page.should have_selector('title', text: "#{base_title}")
-    end
-    it "should not have a custom page title" do
-      visit '/static_pages/home'
-      page.should_not have_selector('title', text: 'Home | ')
-    end    
+    before { visit root_path }
+
+    it { should have_selector('h1',    text: 'Sample App') }
+    it { should have_selector('title', text: full_title('')) }
+    it { should_not have_selector 'title', text: '| Home' }
   end
+
   describe "Help page" do
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('h1', text: 'Help')
-    end
-    it "should have the right title" do
-      visit '/static_pages/help'
-      page.should have_selector('title', text: "Help | #{base_title}")
-    end    
+    before { visit help_path }
+
+    it { should have_selector('h1',    text: 'Help') }
+    it { should have_selector('title', text: full_title('Help')) }
   end
+
   describe "About page" do
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_selector('h1', text: 'About Us')
-    end
-    it "should have the right title" do
-      visit '/static_pages/about'
-      page.should have_selector('title', text: "About Us | #{base_title}")
-    end    
+    before { visit about_path }
+
+    it { should have_selector('h1',    text: 'About') }
+    it { should have_selector('title', text: full_title('About Us')) }
   end
+
   describe "Contact page" do
-    it "should have the content 'Contact Us'" do
-      visit '/static_pages/contact'
-      page.should have_selector('h1', text: 'Contact Us')
-    end
-    it "should have the right title" do
-      visit '/static_pages/contact'
-      page.should have_selector('title', text: "Contact Us | #{base_title}")
-    end    
-  end  
+    before { visit contact_path }
+
+    it { should have_selector('h1',    text: 'Contact') }
+    it { should have_selector('title', text: 'Contact') }
+  end
 end
