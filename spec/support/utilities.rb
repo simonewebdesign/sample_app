2505b42652deb5f438611d072c25a0f2bfcd40c5
Simone <hello@simonewebdesign.it>
2012-12-13 21:40:50 +0100

    Chapter 6 exercises

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,9 +1,15 @@
+include ApplicationHelper
+# it includes the ApplicationHelper module, located in:
+# app/helpers/application_helper.rb
+
+# so you don't need the sh!t below.
+
 # Returns the full title on a per-page basis.
-def full_title(page_title)
-  base_title = "Ruby on Rails Tutorial Sample App"
-  if page_title.empty?
-    base_title
-  else
-    "#{page_title} | #{base_title}"
-  end
-end
+#def full_title(page_title)
+#  base_title = "Ruby on Rails Tutorial Sample App"
+#  if page_title.empty?
+#    base_title
+#  else
+#    "#{page_title} | #{base_title}"
+#  end
+#end
