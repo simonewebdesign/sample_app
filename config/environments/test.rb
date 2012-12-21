be268c9b7307b53e9f4dbbe127d93c295409441c
Simone <hello@simonewebdesign.it>
2012-12-21 23:41:40 +0100

    Finish user signup
    End of chapter 7

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,3 +1,5 @@
+
+
 SampleApp::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
@@ -34,4 +36,10 @@ SampleApp::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  # Speed up tests by lowering BCrypt's cost function.
+  require 'bcrypt'
+  silence_warnings do
+    BCrypt::Engine::DEFAULT_COST = BCrypt::Engine::MIN_COST
+  end  
 end
