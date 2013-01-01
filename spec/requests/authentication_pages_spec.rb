40330462d4c0bda28db35b065fb81d3da3af5850
Simone <hello@simonewebdesign.it>
2013-01-01 14:25:39 +0100

    Revert "Add a pair of spec helpers"
    This reverts commit f1c4ec53e602046e9f30a676620b54d2a270ded0.

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -8,7 +8,7 @@ describe "Authentication" do
     before { visit signin_path }
 
     it { should have_selector('h1',    text: 'Sign in') }
-    it { should have_title('Sign in') }
+    it { should have_selector('title', text: 'Sign in') }
 
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
@@ -19,7 +19,7 @@ describe "Authentication" do
     # end
       before { valid_signin(user) }
 
-      it { should have_title(user.name) }
+      it { should have_selector('title', text: user.name) }
       it { should have_link('Profile', href: user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
@@ -28,7 +28,7 @@ describe "Authentication" do
     describe "with invalid information" do
       before { click_button "Sign in" }
 
-      it { should have_title('Sign in') }
+      it { should have_selector('title', text: 'Sign in') }
       it { should have_error_message('Invalid') }
 
       describe "after visiting another page" do
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -23,15 +23,3 @@ RSpec::Matchers.define :have_error_message do |message|
     page.should have_selector('div.alert.alert-error', text: message)
   end
 end
-
-RSpec::Matchers.define :have_success_message do |message|
-  match do |page|
-    page.should have_selector('div.alert.alert-success', text: message)
-  end
-end
-
-RSpec::Matchers.define :have_title do |title|
-  match do |page|
-    page.should have_title(title)
-  end
-end
