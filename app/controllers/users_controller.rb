331677989aa88a2e46f51561d37d35d4161b8711
Simone <hello@simonewebdesign.it>
2013-01-03 19:40:21 +0100

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
 
+  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_filter :correct_user,   only: [:edit, :update]
+  before_filter :admin_user,     only: :destroy  
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
   def show
     @user = User.find(params[:id])
   end
@@ -21,4 +29,44 @@ class UsersController < ApplicationController
     end    
   end
 
+  def edit
+  #  @user = User.find(params[:id]) already defined by correct_user()
+  end
+
+  def update
+  #  @user = User.find(params[:id]) already defined by correct_user()
+    if @user.update_attributes(params[:user])
+      # Handle a successful update.
+      flash[:success] = "Profile updated"
+      sign_in @user 
+      # this is because the remember token gets reset when the user is saved;
+      # This is a nice security feature, as it means that any hijacked sessions will automatically expire when the user information is changed.
+      redirect_to @user      
+    else
+      render 'edit'
+    end    
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
+  end
+
+  private
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end    
 end
